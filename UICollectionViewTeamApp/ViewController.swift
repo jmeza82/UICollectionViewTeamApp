517de02c762cac4ac04bad00ccd706a1209a1d99
juanmeza <juanmeza255@hotmail.com>
2019-08-23 20:27:17 -0700

    Initial Commit

---
+++ UICollectionViewTeamApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UICollectionViewTeamApp
+//
+//  Created by Juan Meza on 8/23/19.
+//  Copyright © 2019 Juan Meza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
